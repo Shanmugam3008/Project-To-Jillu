8fbb8efabb634d13ad70fc33f6b1140969ff4e16
Shanmugam3008 <shanmugam007143@gmail.com>
2025-08-27 20:30:41 +0530

    Final Update before Production

--- Project-CRUD/Project-Folder-main/client/src/components/Login/index.jsx
+++ Project-CRUD/Project-Folder-main/client/src/components/Login/index.jsx
@@ -61,7 +61,8 @@ const Login = () => {
 				</div>
 				<div className={styles.right}>
 					<h1>New Here ?</h1>
-					<Link to="/signup">
+					<Link to="">
+					{/* <Link to="/signup"></Link> */}
 						<button type="button" className={styles.white_btn}>
 							Sign Up
 						</button>
